4af262621a72d1dfaeacf3b7f321ea1fa91d46a0
Rhenan021389 <rtampus@uic.edu.ph>
2026-09-23 14:10:55 +0800

    Day 1: JS objects - bundling client data

---
+++ lesson3-exercise.js
@@ -0,0 +1,11 @@
+const client = {
+    name: "John",
+    contactNumber: "0991-722-4877",
+    ridingLevel: "Beginner"
+}
+
+console.log (client);
+
+client.ridingLevel = "Advanced"
+
+console.log (client.ridingLevel)
---
+++ lesson3.js
@@ -0,0 +1,9 @@
+const client = {
+  name: 'Jon',
+  ridingLevel: 'Beginner',
+  age: 25
+};
+
+console.log(client.name);
+console.log(client.ridingLevel);
+console.log(client);
