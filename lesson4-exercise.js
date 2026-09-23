468612d066a418d98e49439b8f38628b735bf591
Rhenan021389 <rtampus@uic.edu.ph>
2026-09-23 14:28:43 +0800

    Day 1: JS arrays - indexing, objects in arrays, push

---
+++ lesson4-exercise.js
@@ -0,0 +1,12 @@
+const clients = [
+    {name: "Jon", ridingLevel: "Beginner"},
+    {name: "John", ridingLevel: "Intermediate"},
+    {name: "Jan", ridingLevel: "Advanced"}
+]
+
+console.log (clients)
+console.log (clients[1].name)
+
+clients.push({name: "Jane", ridingLevel: "Advanced"})
+
+console.log (clients.length)
---
+++ lesson4.js
@@ -0,0 +1,6 @@
+const clients = [
+  { name: 'Jon', ridingLevel: 'Beginner' },
+  { name: 'Ana', ridingLevel: 'Intermediate' }
+];
+
+console.log(clients[0].name);
